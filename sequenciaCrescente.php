f820e19d316affd8da63f3aa457b7bbe62843d15
Wanderley Patrício <wpatricio26@gmail.com>
2022-01-07 20:31:10 -0300

    corrigindo codigo

--- sequenciaCrescente.php
+++ sequenciaCrescente.php
@@ -2,6 +2,7 @@
 function sequenciaCrescente($array){
     $cont = 0;
     $aux = 0;
+    $maior = 0;
     
     $array =  explode(',', $array);
     
@@ -11,30 +12,56 @@ function sequenciaCrescente($array){
         array_push($novo_array, intval($v));
     }
     
-    $result_array = array_unique($novo_array);
-    echo sizeof($result_array)." ". sizeof($novo_array);
-    if(sizeof($result_array) === sizeof($novo_array)){
-        for($i = 0; $i < sizeof($novo_array); $i++){
-            if($novo_array[$i] > $novo_array[$i+1]){
+    
+    for($i = 1; $i < sizeof($novo_array); $i++){
+        for($i = 1; $i < sizeof($novo_array); $i++){
+            if($novo_array[$i - 1] >= $novo_array[$i]){
+                $aux = $novo_array[$i - 1];
+                $novo_array[$i - 1] = $novo_array[$i];
+                $novo_array[$i] = $aux;
                 $cont++;
-                $aux = $novo_array[$i];
-                $novo_array[$i] = $novo_array[$i +1];
-                $novo_array[$i + 1] = $aux;        
-
             }
         }
+    }
+    
+    if($cont >= 2){
+        $cont = 0;
+        $novo_array = array();
+        foreach($array as $v){
+            array_push($novo_array, intval($v));
+        }
+        $maior_valor = max($novo_array);
+        $novo_array = array_values(array_diff($novo_array, [$maior_valor]));
+        array_push($novo_array, $maior_valor);
+        for($x = 1; $x < 10; $x++){
+            for($i = 1; $i < sizeof($novo_array); $i++){
+                if($novo_array[$i - 1] >= $novo_array[$i]){
+                    $aux = $novo_array[$i - 1];
+                    $novo_array[$i - 1] = $novo_array[$i];
+                    $novo_array[$i] = $aux;
+                    $cont++;
+                }
+            }
+        }
+            
+        echo json_encode($novo_array);
+        if($cont < 1){
+            echo " true";
+        }else{
+            echo " false";
+        }
+        $cont = 0;
+            
+    }else{
         echo json_encode($novo_array);
         if($cont < 2){
             echo " true";
         }else{
             echo " false";
         }
-        echo $cont;
         $cont = 0;
-    }else{
-        echo json_encode($novo_array);
-        echo " false";
     }
+    
 }
 
 
